8a26da7cf86819f74e7302d117b000a76c36e1ba
Vitalya Volynskiy <i@vitalya.me>
2021-10-15 17:49:06 +0600

    чуть больше деталей в окне настроек

--- packages/renderer/src/components/modals/settings-modal.tsx
+++ packages/renderer/src/components/modals/settings-modal.tsx
@@ -1,14 +1,29 @@
 import { FC } from 'react'
 import { useTranslation } from 'react-i18next'
+import { FormItem, Select } from '@vkontakte/vkui'
 import { ModalProps } from '../modal-container'
 import { ModalHeader } from './modal-header'
+
+import './settings-modal.css'
 
 export const SettingsModal: FC<ModalProps> = () => {
   const { t } = useTranslation()
 
   return (
-    <div className="modal compose-modal">
+    <div className="modal settings-modal">
       <ModalHeader>{t`settings.title`}</ModalHeader>
+      <FormItem top="Тема оформления">
+        <Select
+          // onChange={this.onChange}
+          value="0"
+          name="purpose"
+          options={[
+            { value: '0', label: 'Системная' },
+            { value: '1', label: 'Светлая' },
+            { value: '2', label: 'Темная' },
+          ]}
+        />
+      </FormItem>
     </div>
   )
 }
