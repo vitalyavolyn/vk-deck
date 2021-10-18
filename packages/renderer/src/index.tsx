850574d34551ccf40ffa67484344747e484407b5
Alexander Titov <fox@raidorev.tech>
2021-10-18 10:08:25 +0600

    refactor: использование алиасов на src в .ts файлах

--- packages/renderer/src/index.tsx
+++ packages/renderer/src/index.tsx
@@ -4,9 +4,9 @@ import i18n from 'i18next'
 import { configure } from 'mobx'
 import { initReactI18next } from 'react-i18next'
 import ru from '../locales/ru.yml'
-import { App } from './app'
-import { RootStore } from './store/root-store'
-import { StoreContext } from './store-context'
+import { App } from '@/app'
+import { RootStore } from '@/store/root-store'
+import { StoreContext } from '@/store-context'
 
 import './global.css'
 
