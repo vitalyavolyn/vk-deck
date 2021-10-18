0af07946cbcd0e449c8cbccdc86823fb8e51d8f1
Alexander Titov <fox@raidorev.tech>
2021-10-18 15:52:09 +0600

    style: prettier вместо eslint --fix

--- stylelint.config.js
+++ stylelint.config.js
@@ -2,6 +2,7 @@ module.exports = {
   extends: [
     'stylelint-config-standard',
     'stylelint-config-idiomatic-order',
+    'stylelint-config-prettier',
   ],
   rules: {
     'length-zero-no-unit': [true, { ignore: ['custom-properties'] }],
