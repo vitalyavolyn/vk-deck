bd28c3955869152bf8c7511a10e352960ceee492
Alexander Titov <fox@raidorev.tech>
2021-10-18 10:18:04 +0600

    fix: length-zero-no-unit для кастомных свойств

--- stylelint.config.js
+++ stylelint.config.js
@@ -3,4 +3,7 @@ module.exports = {
     'stylelint-config-standard',
     'stylelint-config-idiomatic-order',
   ],
+  rules: {
+    'length-zero-no-unit': [true, { ignore: ['custom-properties'] }],
+  },
 }
