5dddae22e3c789cb2011c86319e58b4a3beac9f5
Vitaliy Volynskiy <i@vitalya.me>
2021-10-19 17:45:52 +0600

    feat: окно настроек (#3)
    кнопка выхода, настройка темы оформления

--- packages/renderer/src/components/modals/settings-modal.tsx
+++ packages/renderer/src/components/modals/settings-modal.tsx
@@ -1,14 +1,47 @@
-import { FC } from 'react'
+import { ChangeEvent, FC } from 'react'
 import { useTranslation } from 'react-i18next'
+import { Button, FormItem, Select } from '@vkontakte/vkui'
+import { observer } from 'mobx-react-lite'
 import { ModalProps } from '../modal-container'
 import { ModalHeader } from './modal-header'
+import { useStore } from '@/hooks/use-store'
+import { ColorScheme } from '@/store/settings-store'
 
-export const SettingsModal: FC<ModalProps> = () => {
+import './settings-modal.css'
+
+const logout = () => {
+  localStorage.clear()
+  location.reload()
+}
+
+export const SettingsModal: FC<ModalProps> = observer(() => {
   const { t } = useTranslation()
+  const { settingsStore } = useStore()
+
+  const onColorThemeChange = (e: ChangeEvent<HTMLSelectElement>) => {
+    settingsStore.colorScheme = e.target.value as ColorScheme
+  }
 
   return (
-    <div className="modal compose-modal">
+    <div className="modal settings-modal">
       <ModalHeader>{t`settings.title`}</ModalHeader>
+      <FormItem top={t`settings.colorScheme.title`}>
+        <Select
+          onChange={onColorThemeChange}
+          value={settingsStore.colorScheme}
+          options={[
+            { value: 'auto', label: t`settings.colorScheme.auto` },
+            { value: 'light', label: t`settings.colorScheme.light` },
+            { value: 'dark', label: t`settings.colorScheme.dark` },
+          ]}
+        />
+      </FormItem>
+
+      <div className="logout-button-container">
+        <Button stretched mode="secondary" size="m" onClick={logout}>
+          {t`settings.logout`}
+        </Button>
+      </div>
     </div>
   )
-}
+})
--- packages/renderer/src/store/root-store.ts
+++ packages/renderer/src/store/root-store.ts
@@ -1,7 +1,9 @@
 import { UserStore } from './user-store'
 import { SnackbarStore } from './snackbar-store'
+import { SettingsStore } from './settings-store'
 
 export class RootStore {
   public userStore: UserStore = new UserStore(this)
   public snackbarStore: SnackbarStore = new SnackbarStore(this)
+  public settingsStore: SettingsStore = new SettingsStore(this)
 }
