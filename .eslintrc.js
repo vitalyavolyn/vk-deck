0d378593332d17158f61086fe8c1b4351b626710
Vitalya Volynskiy <i@vitalya.me>
2021-12-06 11:18:08 +0600

    фикс ложных ошибок еслинта

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,11 +9,11 @@ module.exports = {
   },
   extends: [
     'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
     'plugin:import/recommended',
     'plugin:import/typescript',
     'plugin:unicorn/recommended',
     'standard',
+    'plugin:@typescript-eslint/recommended',
     'prettier',
   ],
   parser: '@typescript-eslint/parser',
