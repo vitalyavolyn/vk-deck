b690fc097d061e2dfe0e2b938ce462a772499250
Vitalya Volynskiy <i@vitalya.me>
2021-12-14 10:49:14 +0600

    feat: предпросмотр картинок при наведении

--- packages/renderer/src/components/media-grid.tsx
+++ packages/renderer/src/components/media-grid.tsx
@@ -1,6 +1,7 @@
-import { FC } from 'react'
-import { PhotosPhoto } from '@vkontakte/api-schema-typescript'
+import { FC, MouseEventHandler, useState } from 'react'
+import { PhotosPhoto, PhotosPhotoSizes } from '@vkontakte/api-schema-typescript'
 import { classNames } from '@vkontakte/vkjs'
+import { Spinner } from '@vkontakte/vkui'
 import { HasImageGridSettings } from '@/components/columns/common/column-image-grid-settings-form'
 import { useColumn } from '@/hooks/use-column'
 import { useElectron } from '@/hooks/use-electron'
@@ -13,12 +14,20 @@ interface MediaGridProps {
   photos: PhotosPhoto[]
 }
 
+interface Point {
+  x: number
+  y: number
+}
+
 export const MediaGrid: FC<MediaGridProps> = ({ photos }) => {
   const {
     settings: { imageGridSize },
   } = useColumn<HasImageGridSettings>()
   const { openViewer } = useElectron()
   const { apiStore } = useStore()
+
+  const [popupPhoto, setPopupPhoto] = useState<PhotosPhotoSizes | null>(null)
+  const [popupCoordinates, setPopupCoordinates] = useState<Point | null>(null)
 
   const openPhotosInViewer = (index = 0) => {
     openViewer({
@@ -32,36 +41,90 @@ export const MediaGrid: FC<MediaGridProps> = ({ photos }) => {
     })
   }
 
+  const mouseLeave = () => {
+    setPopupCoordinates(null)
+  }
+
+  const mouseEnter = (photo: PhotosPhotoSizes) => {
+    setPopupPhoto(photo)
+  }
+
+  const mouseMove: MouseEventHandler = (e) => {
+    if (!popupPhoto) return
+
+    const { clientX: x, clientY: y } = e
+    const { innerWidth, innerHeight } = window
+
+    let photoHeight, photoWidth
+
+    if (popupPhoto.width / popupPhoto.height < 350 / 500) {
+      const ratio = popupPhoto.width / popupPhoto.height
+
+      photoHeight = Math.min(500, popupPhoto.height)
+      photoWidth = Math.min(350, photoHeight * ratio)
+    } else {
+      const ratio = popupPhoto.height / popupPhoto.width
+
+      photoWidth = Math.min(350, popupPhoto.width)
+      photoHeight = Math.min(500, photoWidth * ratio)
+    }
+
+    setPopupCoordinates({
+      x: innerWidth - photoWidth - x < 0 ? innerWidth - photoWidth : x,
+      y: innerHeight - photoHeight - y < 0 ? innerHeight - photoHeight : y,
+    })
+  }
+
   return (
-    <div
-      className={`media-grid media-grid-${Math.min(photos.length, 6)} media-grid-${imageGridSize}`}
-    >
-      {photos.slice(0, 6).map((photo, i) => {
-        if (!photo || !photo.sizes) return null // TODO: проблема тайпинга?
-
-        const sortedSizes = photo.sizes.sort((a, b) => a.height - b.height)
-
-        const minWidth = 240
-        const filteredSizes = sortedSizes.filter((e) => e.width > minWidth)
-        const { url } = filteredSizes.length
-          ? filteredSizes[0]
-          : sortedSizes[sortedSizes.length - 1]
-
-        const isOverlayed = photos.length > 6 && i === 5
-
-        return (
-          <div
-            key={`${photo?.owner_id}_${photo?.id}`}
-            onClick={() => !isOverlayed && openPhotosInViewer(i)}
-            className={classNames('img', {
-              'has-more': isOverlayed,
-            })}
-            data-text={`+${photos.length - 5}`}
-          >
-            <img src={url} />
+    <>
+      {popupCoordinates && popupPhoto && (
+        <div className="photo-popup" style={{ top: popupCoordinates.y, left: popupCoordinates.x }}>
+          <div className="spinner-wrap">
+            {/* TODO: первые полсекунды находится у курсора */}
+            <Spinner size="medium" />
           </div>
-        )
-      })}
-    </div>
+          <div className="image-wrap">
+            <img src={popupPhoto.url} />
+          </div>
+        </div>
+      )}
+      <div
+        className={`media-grid media-grid-${Math.min(
+          photos.length,
+          6,
+        )} media-grid-${imageGridSize}`}
+      >
+        {photos.slice(0, 6).map((photo, i) => {
+          if (!photo || !photo.sizes) return null // TODO: проблема тайпинга?
+
+          const sortedSizes = photo.sizes.sort((a, b) => a.height - b.height)
+
+          const minWidth = 240
+          const filteredSizes = sortedSizes.filter((e) => e.width > minWidth)
+          const { url } = filteredSizes.length
+            ? filteredSizes[0]
+            : sortedSizes[sortedSizes.length - 1]
+
+          const isOverlayed = photos.length > 6 && i === 5
+
+          return (
+            <div
+              key={`${photo?.owner_id}_${photo?.id}`}
+              onClick={() => !isOverlayed && openPhotosInViewer(i)}
+              className={classNames('img', {
+                'has-more': isOverlayed,
+              })}
+              data-text={`+${photos.length - 5}`}
+              // TODO: выбирать не самый большой размер лол
+              onMouseEnter={() => mouseEnter(sortedSizes[sortedSizes.length - 1])}
+              onMouseLeave={mouseLeave}
+              onMouseMove={mouseMove}
+            >
+              <img src={url} />
+            </div>
+          )
+        })}
+      </div>
+    </>
   )
 }
