64211204e28ee8fd30f561317a2ef75ef10748e4
Vitaliy Volynskiy <i@vitalya.me>
2021-12-22 10:57:12 +0600

    feat: обязательное заполнение запроса перед добавлением колонки поиска (#14)

---
+++ packages/renderer/src/components/column-setup/newsfeed-search-column-setup.tsx
@@ -0,0 +1,35 @@
+import { FC, useState } from 'react'
+import { Button, FormItem, FormLayout, Input } from '@vkontakte/vkui'
+import { useTranslation } from 'react-i18next'
+import { SetupProps } from '@/components/modals/add-column-modal'
+import { ColumnType } from '@/store/settings-store'
+
+export const NewsfeedSearchColumnSetup: FC<SetupProps> = ({ addColumn }) => {
+  const { t } = useTranslation()
+  const [query, setQuery] = useState('')
+
+  return (
+    <>
+      <FormLayout>
+        <FormItem>
+          <Input
+            placeholder={t`newsfeedSearchSetup.placeholder`}
+            onChange={(e) => {
+              setQuery(e.target.value)
+            }}
+          />
+        </FormItem>
+      </FormLayout>
+      <Button
+        disabled={!query}
+        onClick={() => {
+          addColumn(ColumnType.newsfeedSearch, {
+            query,
+          })
+        }}
+      >
+        {t`addColumn.title`}
+      </Button>
+    </>
+  )
+}
